cc51031b00ad3c9a018ae3308d17693facafa868
Adrien <adrien.cleach@gmail.com>
2026-01-22 16:29:53 +0100

    fix: update cloudinary config structure

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,8 +1,18 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Cloudinary Configuration
+|--------------------------------------------------------------------------
+*/
+
 return [
-    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    'api_key' => env('CLOUDINARY_API_KEY'),
-    'api_secret' => env('CLOUDINARY_API_SECRET'),
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+    'cloud_url' => env('CLOUDINARY_URL'),
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
     'secure' => true,
 ];
