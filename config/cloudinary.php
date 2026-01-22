18a0a53ce704165ef5381e42cbb78751c35229dc
Adrien <adrien.cleach@gmail.com>
2026-01-22 16:39:05 +0100

    fix: flatten Cloudinary config structure (remove nested cloud array)

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -7,12 +7,11 @@
 */
 
 return [
-    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
-    'cloud_url' => env('CLOUDINARY_URL'),
-    'cloud' => [
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key' => env('CLOUDINARY_API_KEY'),
-        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+    'api_key' => env('CLOUDINARY_API_KEY'),
+    'api_secret' => env('CLOUDINARY_API_SECRET'),
+    
+    'url' => [
+        'secure' => true,
     ],
-    'secure' => true,
 ];
